7a58ea2d8bd184e7a09826a0d1c3723cb550cba3
sebastian-rebeiro <96997159+sebastian-rebeiro@users.noreply.github.com>
2022-09-13 21:39:17 -0400

    working rocket api with freeside

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,44 @@
 #[macro_use]
 extern crate rocket;
 
+use reqwest::Response;
+// use serde::{Deserialize, Serialize};
+// use serde_xml_rs::{from_str, to_string};
+
+async fn simple_post(url: String, body: String) -> Response {
+    let response = reqwest::Client::new()
+        .post(url)
+        .body(body)
+        .send()
+        .await
+        .expect("Send Unssucsessful");
+    response
+}
+
+// #[derive(, Serialize, Deserialize, PartialEq)]
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/hello/<name>/<age>")]
-fn hello(name: String, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
+#[get("/client/<clientnum>")]
+async fn hello(clientnum: u32) -> String {
+    let uri: String = String::from("http://billing.dido.ca:8008");
+
+    let front: String = String::from("<methodCall><methodName>FS.API.customer_info</methodName><params><param><value><string>secret</string></value></param><param><value><string></string></value></param><param><value><string>custnum</string></value></param><param><value><string>");
+    let back: String = String::from("</string></value></param></params></methodCall>");
+
+    let post = format!("{front}{clientnum}{back}");
+
+    simple_post(uri, post)
+        .await
+        .text()
+        .await
+        .expect("error reading")
 }
 
-fn main() {
-    rocket::ignite()
-        .mount("/api/v1/", routes![index, hello])
-        .launch();
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/api/v1/", routes![index, hello])
 }
