4e102eaf30c336c37c256af03e44da6d5ffef4f6
sebastian-rebeiro <96997159+sebastian-rebeiro@users.noreply.github.com>
2022-09-18 17:22:28 -0400

    new_customer works!.. ugly code though..

--- src/main.rs
+++ src/main.rs
@@ -90,11 +90,10 @@ enum Param {
     salesnum,
     agentnum,
     agent_custid,
-    refferal_custnum,
 }
 
-#[derive(Debug, Deserialize)]
-struct inp_customer {
+#[derive(Debug, Deserialize, FromForm)]
+struct InpCustomer {
     first: String,
     last: String,
     ss: String,
@@ -105,7 +104,7 @@ struct inp_customer {
     state: String,
     zip: String,
     country: String,
-    latituge: String,
+    latitude: String,
     longitude: String,
     geocode: String,
     censustract: String,
@@ -165,25 +164,135 @@ async fn customer_info(clientnum: u32, apisecret: fromreq::ApiKey) -> content::R
     content::RawJson(simple_post(&URI, post).await)
 }
 
-#[get("/sensitive")]
-fn sensitive(key: fromreq::ApiKey) -> String {
-    format!("this key is valid {}", key.0)
-}
-
 #[route(POST, uri = "/client", data = "<data>")]
-async fn new_customer(data: Json<String>, apisecret: fromreq::ApiKey) -> content::RawJson<String> {
-    println!("{:?}", data);
+async fn new_customer(
+    data: Json<InpCustomer>,
+    apisecret: fromreq::ApiKey,
+) -> content::RawJson<String> {
+    println!("{:?}", &data);
 
     let post: String = outgoing_body(
-        Methods::customer_info,
+        Methods::new_customer,
         vec![
             Params {
                 name: Param::secret,
                 value: apisecret.0,
             },
             Params {
-                name: Param::custnum,
-                value: String::from(""),
+                name: Param::first,
+                value: data.0.first,
+            },
+            Params {
+                name: Param::last,
+                value: data.0.last,
+            },
+            Params {
+                name: Param::ss,
+                value: data.0.ss,
+            },
+            Params {
+                name: Param::company,
+                value: data.0.company,
+            },
+            Params {
+                name: Param::address1,
+                value: data.0.address1,
+            },
+            Params {
+                name: Param::city,
+                value: data.0.city,
+            },
+            Params {
+                name: Param::county,
+                value: data.0.county,
+            },
+            Params {
+                name: Param::state,
+                value: data.0.state,
+            },
+            Params {
+                name: Param::zip,
+                value: data.0.zip,
+            },
+            Params {
+                name: Param::country,
+                value: data.0.country,
+            },
+            Params {
+                name: Param::latitude,
+                value: data.0.latitude,
+            },
+            Params {
+                name: Param::longitude,
+                value: data.0.longitude,
+            },
+            Params {
+                name: Param::geocode,
+                value: data.0.geocode,
+            },
+            Params {
+                name: Param::censustract,
+                value: data.0.censustract,
+            },
+            Params {
+                name: Param::censusyear,
+                value: data.0.censusyear,
+            },
+            Params {
+                name: Param::daytime,
+                value: data.0.daytime,
+            },
+            Params {
+                name: Param::night,
+                value: data.0.night,
+            },
+            Params {
+                name: Param::fax,
+                value: data.0.fax,
+            },
+            Params {
+                name: Param::mobile,
+                value: data.0.mobile,
+            },
+            Params {
+                name: Param::invoicing_list,
+                value: data.0.invoicing_list,
+            },
+            Params {
+                name: Param::payby,
+                value: data.0.payby,
+            },
+            Params {
+                name: Param::payinfo,
+                value: data.0.payinfo,
+            },
+            Params {
+                name: Param::paycvv,
+                value: data.0.paycvv,
+            },
+            Params {
+                name: Param::paydate,
+                value: data.0.paydate,
+            },
+            Params {
+                name: Param::payname,
+                value: data.0.payname,
+            },
+            Params {
+                name: Param::referral_custnum,
+                value: data.0.referral_custnum,
+            },
+            Params {
+                name: Param::salesnum,
+                value: data.0.salesnum,
+            },
+            Params {
+                name: Param::agentnum,
+                value: data.0.agentnum,
+            },
+            Params {
+                name: Param::agent_custid,
+                value: data.0.agent_custid,
             },
         ],
     );
